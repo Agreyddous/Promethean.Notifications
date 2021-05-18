78fb0af434ff7c6c5991d269c3af7fbee8d335b2
Nando <fernandovbmgomes@hotmail.com>
2021-05-18 11:06:12 -0300

    Convenience

--- Promethean.Notifications/Validators/Validator.cs
+++ Promethean.Notifications/Validators/Validator.cs
@@ -1,4 +1,7 @@
 namespace Promethean.Notifications.Validators
 {
-	public partial class Validator : Notifiable { }
+	public partial class Validator : Notifiable
+	{
+		public static implicit operator bool(Validator validator) => validator.Valid;
+	}
 }
